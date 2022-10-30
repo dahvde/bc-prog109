7657dc8d4e9024a976b34e094a2708962f47e9e2
dahvde <owlsensation@gmail.com>
2022-10-30 02:00:04 -0700

    Updated Artifact 3 Assignment (Massive Changes)

--- Artifact_3/main.js
+++ Artifact_3/main.js
@@ -1,5 +1,6 @@
 var delay;
 var element = document.body;
+var checkBoxes = document.getElementsByClassName("checkmark");
 
 // default dark mode
 element.classList.toggle("dark-mode");
@@ -22,7 +23,7 @@ function createRhombus() {
       .repeat(height - i)
       .split("")
       .map((e) => {
-        return `<p colorValue="rgb(112, 126, 251)">${e}</p>`;
+        return `<p class="transition">${e}</p>`;
       })
       .join("");
 
@@ -46,7 +47,7 @@ function createRhombus() {
 
   document.getElementById("rhombus").innerHTML = rhombus
     .reverse()
-    .concat([...rhombus].reverse())
+    .concat([...rhombus].reverse().splice(1, rhombus.length))
     .join("<br>");
 }
 
@@ -62,22 +63,112 @@ function setColor() {
     "--color-odd",
     document.getElementById("colorOdd").value
   );
+
+  saveColor();
 }
 
 // add delay to the rhombus creation on input change
 document.getElementById("rHeight").addEventListener("input", function () {
   clearTimeout(delay);
+  if (this.value >= 130) this.value = 130;
+  if (this.value < 0) this.value = 1;
   delay = setTimeout(function () {
     createRhombus();
   }, 200);
 });
 
 function toggleColorMode() {
-  if (element.classList[0] == "light-mode") {
-    element.classList.toggle("light-mode");
+  const colorToggle = document.querySelector("#colorToggle > img");
+
+  // init color mode
+  if (localStorage.getItem("colorMode") === "light") {
+    localStorage.setItem("colorMode", "dark");
+    document.body.classList = "dark-mode";
+    colorToggle.setAttribute("src", "./images/sun.svg");
   } else {
-    element.classList.toggle("dark-mode");
+    localStorage.setItem("colorMode", "light");
+    document.body.classList = "light-mode";
+    colorToggle.setAttribute("src", "./images/moon.svg");
   }
 }
 
+function initLocalStorageItems() {
+  const colorEven = document.getElementById("colorEven");
+  const colorOdd = document.getElementById("colorOdd");
+  const colorEvenValue = localStorage.getItem("colorEven");
+  const colorOddValue = localStorage.getItem("colorOdd");
+
+  if (localStorage.getItem("colorMode") === "light") {
+    document.body.classList = "light-mode";
+    document
+      .querySelector("#colorToggle > img")
+      .setAttribute("src", "./images/moon.svg");
+  } else {
+    document.body.classList = "dark-mode";
+    document
+      .querySelector("#colorToggle > img")
+      .setAttribute("src", "./images/sun.svg");
+  }
+  if (colorEvenValue) {
+    colorEven.value = colorEvenValue;
+  } else {
+    saveColor();
+  }
+  if (colorOddValue) {
+    colorOdd.value = colorOddValue;
+  } else {
+    saveColor();
+  }
+}
+
+function saveColor() {
+  const colorEven = document.getElementById("colorEven");
+  const colorOdd = document.getElementById("colorOdd");
+
+  console.log(colorEven.value);
+
+  localStorage.setItem("colorEven", colorEven.value);
+
+  localStorage.setItem("colorOdd", colorOdd.value);
+}
+
+// init transition on page load
+function initTranstion() {
+  const colorButton = document.getElementById("colorToggle");
+  const sidebar = document.getElementById("input-wrapper");
+
+  setTimeout(() => {
+    colorButton.classList.add("transition");
+    sidebar.classList.add("transition");
+    document.body.classList.add("transition");
+  }, 1000);
+}
+
+Array.from({ length: checkBoxes.length }).forEach((e, i) => {
+  checkBoxes[i].addEventListener("click", (e) => {
+    const inputBox = e.target.parentElement.parentElement.children[1];
+    const checked = inputBox.getAttribute("checked");
+    const rhombus = document.getElementById("rhombus");
+    const $Attr = inputBox.getAttribute("data");
+
+    if (checked != null) {
+      inputBox.removeAttribute("checked");
+      rhombus.classList.add($Attr);
+      // Lock hidden class to single element
+      if (e.target.tagName == "IMG") {
+        e.target.classList.add("hidden");
+      }
+    } else {
+      inputBox.setAttribute("checked", true);
+      rhombus.classList.remove($Attr);
+      // Lock hidden class to single element
+      if (e.target.tagName == "IMG") {
+        e.target.classList.remove("hidden");
+      }
+    }
+  });
+});
+
+initTranstion();
+initLocalStorageItems();
 createRhombus();
